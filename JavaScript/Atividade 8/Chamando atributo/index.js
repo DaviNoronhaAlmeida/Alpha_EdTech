81a62cccc2895838dca189db01f597da4e5c8f29
Davi Noronha <davinoronhaalmeida@gmail.com>
2022-06-15 09:57:04 -0300

    atv8 js

--- JavaScript/Atividade 8/Chamando atributo/index.js
+++ JavaScript/Atividade 8/Chamando atributo/index.js
@@ -1,8 +1,9 @@
-let object = {};
+const object = {};
 object['Um atributo com espaços'] = 1;
 
 console.log("Com colchetes:")
 console.log(object['Um atributo com espaços']);
 
 console.log("Sem colchetes:");
+Object.defineProperty(object, "Um atributo com espaços", { value: 2 });
 console.log(object);
